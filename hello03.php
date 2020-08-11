05df439f7c2e1c1b312ce6379b56a7c8786e83fe
haru125 <jhonathan1471@gmail.com>
2020-08-11 09:08:55 +0900

    hello2

---
+++ hello03.php
@@ -0,0 +1,38 @@
+<?php
+// 課題１
+// $name = "太田都寿";
+// $name = "ああ";
+
+// if ($name == "太田都寿") {
+//   echo "私は", $name , "です";
+// } else {
+//   echo "あなたの名前ではありません";
+// }
+
+// 課題２
+for ($i = 0; $i <= 10000; $i++) {
+  $total += $i;
+}
+echo $total;
+
+// 課題３
+// $fruits = array("strawberry", "cherry", "peach", "grape", "banana");
+
+// foreach($fruits as $fruit){
+//   echo $fruit;
+//   echo "\n";
+// }
+
+// 課題４
+// $start = 1;
+// $end = 100;
+
+// for($i = $start; $i <= $end; $i++) {
+//   if($i % 5 == 0){
+//     echo $i;
+//     echo "\n";
+//   }
+// }
+
+
+?>
